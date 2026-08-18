d191357418771491d83b533d1ee8a1d72a557fe3
MatheusJacobD <matheus.duarte@sptech.school>
2026-08-18 08:30:09 -0300

    feat: Esconder/mostrar senha

--- Public/js/script.js
+++ Public/js/script.js
@@ -8,3 +8,16 @@ function modalCadastro() {
 fecharCadastro.onclick = function() {
     modal.style.display = "none";
 }
+
+function mostrarSenha(inputId, eyeId) {
+    var input = document.getElementById(inputId);
+    var eye = document.getElementById(eyeId);
+
+    if (input.type === "password") {
+        input.type = "text";
+        eye.src = "../imagens/openEye.png";
+    } else {
+        input.type = "password";
+        eye.src = "../imagens/closedEye.png";
+    }
+}

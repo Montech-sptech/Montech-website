4c8fb45dc01d4ae546f6243b630ee323b611e4ac
MatheusJacobD <matheus.duarte@sptech.school>
2026-08-18 08:51:01 -0300

    feat: Tela de cadastro e login finalizados

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,7 +1,20 @@
 var modal = document.getElementById("modalCadastro");
 var fecharCadastro = document.getElementById("fecharModalCadastro");
 
+var modalL = document.getElementById("modalLogin");
+var fecharLogin = document.getElementById("fecharModalLogin");
+
+function modalLogin() {
+    modal.style.display = "none";
+    modalL.style.display = "flex";
+}
+
+fecharLogin.onclick = function() {
+    modalL.style.display = "none";
+}
+
 function modalCadastro() {
+    modalL.style.display = "none";
     modal.style.display = "flex";
 }
 
@@ -13,7 +26,7 @@ function mostrarSenha(inputId, eyeId) {
     var input = document.getElementById(inputId);
     var eye = document.getElementById(eyeId);
 
-    if (input.type === "password") {
+    if (input.type == "password") {
         input.type = "text";
         eye.src = "../imagens/openEye.png";
     } else {

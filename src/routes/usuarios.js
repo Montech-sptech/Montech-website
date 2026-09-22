f6edee67873dc2d83bc3d6e6a6bf1546bdbc71de
guilhermebrtt <guilherme.britto@sptech.school>
2026-09-21 23:05:49 -0300

    fix: Atualizando Routes de Usuários, Servidores e Empresas

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -5,33 +5,29 @@ var autorizacaoCargo = require("../middleware/autorizacaoCargo");
 var usuarioController = require("../controllers/usuarioController");
 
 router.post("/cadastrar", function (req, res) {
-    usuarioController.cadastrar(req, res);
-})
+  usuarioController.cadastrar(req, res);
+});
 
 router.post("/autenticar", function (req, res) {
-    usuarioController.autenticar(req, res);
+  usuarioController.autenticar(req, res);
 });
 
 router.get("/pegarUsuariosPeloAdministrador/:id", function (req, res) {
-    autorizacaoCargo.verificarAdministrador(req, res, () => {
-        usuarioController.pegarUsuariosPeloAdministrador(req, res);
-    });
+  //autorizacaoCargo.verificarAdministrador(req, res, () => {
+  usuarioController.pegarUsuariosPeloAdministrador(req, res);
 });
+//});
 
 router.put("/inativarUsuario/:id", function (req, res) {
-    autorizacaoCargo.verificarAdministrador(req, res, () => {
-        usuarioController.inativarUsuario(req, res);
-    });
+  //autorizacaoCargo.verificarAdministrador(req, res, () => {
+  usuarioController.inativarUsuario(req, res);
 });
+//});
 
 router.put("/ativarUsuario/:id", function (req, res) {
-    autorizacaoCargo.verificarAdministrador(req, res, () => {
-        usuarioController.ativarUsuario(req, res);
-    });
+  //autorizacaoCargo.verificarAdministrador(req, res, () => {
+  usuarioController.ativarUsuario(req, res);
 });
-
-router.put("/atualizarFotoPerfil/:id", function (req, res) {
-    usuarioController.atualizarFotoPerfil(req, res);
-});
+//});
 
 module.exports = router;

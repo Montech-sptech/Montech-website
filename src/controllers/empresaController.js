6f2230ff5b5d503f69cb4de7e00c3779aa999036
guilhermebrtt <guilherme.britto@sptech.school>
2026-09-21 23:00:07 -0300

    fix: Atualizando Controller de Empresa

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -1,166 +1,88 @@
 var empresaModel = require("../models/empresaModel");
 
-function listar(req, res) {
-
-    empresaModel.listar()
-        .then(function (resultado) {
-
-            res.json(resultado);
-
-        })
-        .catch(function (erro) {
-
-            console.log(erro);
-
-            res.status(500).json(erro.sqlMessage);
-        });
-}
-
 function verificarCadastrados(req, res) {
-    empresaModel.verificarCadastrados()
-        .then(function (resultado) {
+  empresaModel
+    .verificarCadastrados()
+    .then(function (resultado) {
+      res.json(resultado);
+    })
+    .catch(function (erro) {
+      console.log(erro);
 
-            res.json(resultado);
-
-        })
-        .catch(function (erro) {
-
-            console.log(erro);
-
-            res.status(500).json(erro.sqlMessage);
-        });
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
 function carregarEmpresas(req, res) {
+  empresaModel
+    .listarEmpresas()
+    .then(function (resultado) {
+      res.status(200).json(resultado);
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log("Erro ao buscar empresas cadastradas");
 
-    empresaModel.carregarEmpresas()
-        .then(function (resultado) {
-
-            res.status(200).json(resultado);
-
-        })
-        .catch(function (erro) {
-
-            console.log(erro);
-            console.log("Erro ao buscar empresas cadastradas");
-
-            res.status(500).json(erro.sqlMessage);
-        });
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
 async function cadastrar(req, res) {
-    console.log("Acessei o empresaController - cadastrar");
+  console.log("Acessei o empresaController - cadastrar");
 
-    var razaoSocial = req.body.razaoSocialServer;
-    var cnpj = req.body.cnpjServer;
-    var cep = req.body.cepServer;
-    var numero = req.body.numeroServer;
+  var razaoSocial = req.body.razaoSocialServer;
+  var cnpj = req.body.cnpjServer;
+  var email = req.body.emailServer;
+  var cep = req.body.cepServer;
+  var numero = req.body.numeroServer;
 
-    if (!razaoSocial) {
-        res.status(400).send("A Razão social está vazia ou undefined!");
-    } else if (!cnpj) {
-        res.status(400).send("O CNPJ está vazio ou undefined!");
-    } else if (!cep) {
-        res.status(400).send("O CEP está vazio ou undefined!");
-    } else if (!numero) {
-        res.status(400).send("O Número está vazio ou undefined!");
-    } else {
+  if (!razaoSocial) {
+    res.status(400).send("A Razão social está vazia ou undefined!");
+  } else if (!cnpj) {
+    res.status(400).send("O CNPJ está vazio ou undefined!");
+  } else if (!email) {
+    res.status(400).send("O Email está vazio ou undefined!");
+  } else if (!cep) {
+    res.status(400).send("O CEP está vazio ou undefined!");
+  } else if (!numero) {
+    res.status(400).send("O Número está vazio ou undefined!");
+  } else {
+    try {
+      let resultadoCnpj = await empresaModel.verificarCnpj(cnpj);
 
-        try {
-            let resultadoCnpj = await empresaModel.verificarCnpj(cnpj);
+      if (
+        resultadoCnpj &&
+        resultadoCnpj.length > 0 &&
+        resultadoCnpj[0].qtdCnpj > 0
+      ) {
+        res.status(400).send("CNPJ já cadastrado!");
+        return;
+      }
 
-            if (resultadoCnpj && resultadoCnpj.length > 0 && resultadoCnpj[0].qtdCnpj > 0) {
-                res.status(400).send("CNPJ já cadastrado!");
-                return;
-            }
-
-            let codigoValido = false;
-            let codigo = "";
-
-            while (!codigoValido) {
-                const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                codigo = 'MNT';
-
-                for (let i = 3; i < 8; i++) {
-                    codigo += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-                }
-
-                console.log('Código gerado: ', codigo);
-
-                let resultado = await empresaModel.verificarCodigoCadastro(codigo);
-
-                if (resultado && resultado.length > 0 && resultado[0].qtdEmpresa == 0) {
-                    codigoValido = true;
-                    console.log('Código válido');
-                    break;
-                } else {
-                    console.log("Código inválido, tentando novamente");
-                }
-            }
-
-            empresaModel.cadastrar(razaoSocial, cnpj, codigo, cep, numero)
-                .then(function (resultado) {
-                    res.json({
-                        empresa_id: resultado.insertId,
-                        codigo_empresa: codigo
-                    });
-                })
-                .catch(function (erro) {
-                    console.log("\nHouve erro ao cadastrar empresa! ERRO: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                });
-
-        } catch (erro) {
-            console.log("\nErro na verificação de dados: ", erro);
-            res.status(500).json("Erro interno no servidor.");
-        }
+      empresaModel
+        .cadastrar(razaoSocial, cnpj, email, cep, numero)
+        .then(function (resultado) {
+          res.json({
+            empresa_id: resultado.insertId,
+            codigo_empresa: codigo,
+          });
+        })
+        .catch(function (erro) {
+          console.log(
+            "\nHouve erro ao cadastrar empresa! ERRO: ",
+            erro.sqlMessage,
+          );
+          res.status(500).json(erro.sqlMessage);
+        });
+    } catch (erro) {
+      console.log("\nErro na verificação de dados: ", erro);
+      res.status(500).json("Erro interno no servidor.");
     }
-}
-
-function mensagensContatos(req, res) {
-
-    empresaModel.mensagensContatos()
-        .then(function (resultado) {
-            res.status(200).json(resultado);
-        })
-        .catch(function (erro) {
-            console.log(erro);
-            console.log("Erro ao buscar mensagens de contato");
-            res.status(500).json(erro.sqlMessage);
-        });
-}
-
-function contato(req, res) {
-
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var telefone = req.body.telefoneServer;
-
-    if (!nome) {
-       return res.status(400).send("O nome está vazio ou undefined!");
-    } else if (!email) {
-       return res.status(400).send("O Email está vazio ou undefined!");
-    } else if (!telefone) {
-       return res.status(400).send("O telefone está vazio ou undefined!");
-    } else {
-
-    empresaModel.contato(nome, email, telefone)
-        .then(function (resultado) {
-            res.status(200).json(resultado);
-        })
-        .catch(function (erro) {
-            console.log(erro);
-            console.log("Erro ao cadastrar o contato da empresa");
-            res.status(500).json(erro.sqlMessage);
-        });
-}
+  }
 }
 
 module.exports = {
-    listar,
-    verificarCadastrados,
-    carregarEmpresas,
-    cadastrar,
-    mensagensContatos,
-    contato
+  verificarCadastrados,
+  carregarEmpresas,
+  cadastrar,
 };

de094b5ec2617ee48fe698395342ba016af4acba
guilhermebrtt <guilherme.britto@sptech.school>
2026-09-21 21:32:13 -0300

    fix: Atualizando campos do banco de Servidor no Modal

--- src/models/servidoresModel.js
+++ src/models/servidoresModel.js
@@ -1,71 +1,102 @@
 var database = require("../database/config");
 
 function adicionarServidoresUsuario(idUsuario, idServidor) {
-    var instrucaoSql = `
+  var instrucaoSql = `
         INSERT INTO usuarioServidor (fkUsuario, fkServidor) VALUES (${idUsuario}, ${idServidor});
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function removerServidorUsuario(idUsuario, idServidor) {
-    var instrucaoSql = `
+  var instrucaoSql = `
         DELETE FROM usuarioServidor WHERE fkUsuario = ${idUsuario} AND fkServidor = ${idServidor};
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function pegarServidoresPorEmpresa(idEmpresa) {
-    var instrucaoSql = `
+  var instrucaoSql = `
         SELECT 
-    s.idServidor,
-    s.nomeServidor,
-    s.hostName,
-    s.tipoServidor,
-    s.metodoDeColeta,
-    s.intervaloDeColeta
-FROM servidor s
-ORDER BY s.idServidor;
+            s.idServidor,
+            s.nomeServidor,
+            s.hostName,
+            s.tipoServidor,
+            s.metodoDeColeta,
+            s.intervaloDeColeta
+        FROM servidor s
+        WHERE s.fkEmpresa = ${idEmpresa}
+        ORDER BY s.idServidor;
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
-function cadastrarServidor(nomeServidor, hostName, tipoServidor, metodoDeColeta, intervaloDeColeta) {
-    var hostNameSql = hostName ? `'${hostName}'` : "NULL";
-    var tipoServidorSql = tipoServidor ? `'${tipoServidor}'` : "NULL";
-    var metodoDeColetaSql = metodoDeColeta ? `'${metodoDeColeta}'` : "NULL";
-    var intervaloDeColetaSql = intervaloDeColeta ? intervaloDeColeta : "NULL";
+function cadastrarServidor(
+  nomeServidor,
+  hostName,
+  sistemaOperacional,
+  metodoColeta,
+  intervaloColeta,
+  statusAtividade,
+) {
+  var hostNameSql = hostName ? `'${hostName}'` : "NULL";
+  var sistemaOperacionalSql = sistemaOperacional
+    ? `'${sistemaOperacional}'`
+    : "NULL";
+  var metodoDeColetaSql = metodoColeta ? `'${metodoColeta}'` : "NULL";
+  var intervaloDeColetaSql = intervaloColeta ? intervaloColeta : "NULL";
+  var statusAtividadeSql =
+    statusAtividade !== undefined && statusAtividade !== null
+      ? statusAtividade
+      : "NULL";
 
-    var instrucaoSql = `
-        INSERT INTO servidor (nomeServidor, hostName, tipoServidor, metodoDeColeta, intervaloDeColeta)
-        VALUES ('${nomeServidor}', ${hostNameSql}, ${tipoServidorSql}, ${metodoDeColetaSql}, ${intervaloDeColetaSql});
+  var instrucaoSql = `
+        INSERT INTO servidor (nomeServidor, hostName, sistemaOperacional, metodoColeta, intervaloColeta, statusAtividade)
+        VALUES ('${nomeServidor}', ${hostNameSql}, ${sistemaOperacionalSql}, ${metodoDeColetaSql}, ${intervaloDeColetaSql}, ${statusAtividadeSql});
     `;
-    return database.executar(instrucaoSql);
+  return database.executar(instrucaoSql);
 }
 
 function buscarIdComponentePorNome(nomeComponente) {
-    var instrucaoSql = `SELECT idComponente FROM componente WHERE nomeComponente = '${nomeComponente}';`;
-    return database.executar(instrucaoSql);
+  var instrucaoSql = `SELECT idComponente FROM componente WHERE nomeComponente = '${nomeComponente}';`;
+  return database.executar(instrucaoSql);
 }
 
-function adicionarComponenteServidor(fkComponente, fkServidor, limiteAtencao, limiteCritico) {
-    var limiteAtencaoSql = (limiteAtencao !== undefined && limiteAtencao !== null && limiteAtencao !== "") ? limiteAtencao : "NULL";
-    var limiteCriticoSql = (limiteCritico !== undefined && limiteCritico !== null && limiteCritico !== "") ? limiteCritico : "NULL";
+function adicionarComponenteServidor(
+  fkComponente,
+  fkServidor,
+  limiteAtencao,
+  limiteCritico,
+  fkTipoMetrica,
+  componentePrincipal,
+) {
+  var limiteAtencaoSql =
+    limiteAtencao !== undefined &&
+    limiteAtencao !== null &&
+    limiteAtencao !== ""
+      ? limiteAtencao
+      : "NULL";
+  var limiteCriticoSql =
+    limiteCritico !== undefined &&
+    limiteCritico !== null &&
+    limiteCritico !== ""
+      ? limiteCritico
+      : "NULL";
 
-    var instrucaoSql = `
-        INSERT INTO componenteServidor (fkComponente, fkServidor, limiteAtencao, limiteCritico)
-        VALUES (${fkComponente}, ${fkServidor}, ${limiteAtencaoSql}, ${limiteCriticoSql});
-    `;
-    return database.executar(instrucaoSql);
+  var instrucaoComponenteServidor = `
+        INSERT INTO componenteServidor (fkComponente, fkServidor, fkTipoMetrica, componentePrincipal, limiteAtencao, limiteCritico)
+        VALUES (${fkComponente}, ${fkServidor}, ${fkTipoMetrica}, ${componentePrincipal}, ${limiteAtencaoSql}, ${limiteCriticoSql});
+  `;
+  return database.executar(instrucaoComponenteServidor);
 }
 
 module.exports = {
-    adicionarServidoresUsuario,
-    removerServidorUsuario,
-    pegarServidoresPorEmpresa,
-    cadastrarServidor,
-    buscarIdComponentePorNome,
-    adicionarComponenteServidor
+  adicionarServidoresUsuario,
+  removerServidorUsuario,
+  pegarServidoresPorEmpresa,
+  cadastrarServidor,
+  buscarIdComponentePorNome,
+  adicionarComponenteServidor,
 };
